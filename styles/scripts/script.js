3b2a8b99e751cc4abccd3beff30887e4bedb8db6
ashwin <alexwin75@gmail.com>
2020-10-15 20:42:57 -0400

    making footer responsive

---
+++ styles/scripts/script.js
@@ -0,0 +1,9 @@
+/* Open*/
+function openNav() {
+  document.getElementById("overlay-nav").style.height = "100%";
+}
+
+/* Close*/
+function closeNav() {
+  document.getElementById("overlay-nav").style.height = "0%";
+}
